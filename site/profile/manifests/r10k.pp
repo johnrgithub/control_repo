5e34d1bc83011ffc4b8673ca51eb56b8e71ea5e7
johnrgithub <47810662+johnrgithub@users.noreply.github.com>
2023-12-21 08:27:16 +0800

    Update r10k.pp
    Removed webhook config

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,12 +2,4 @@ class profile::r10k {
   class {'r10k':
     remote => 'git@github.com:johnrgithub/control_repo.git',
   }
-  class {'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl      => false,
-  }
-  class {'r10k::webhook':
-    user  => 'root',
-    group => 'root',
-  }
 }
